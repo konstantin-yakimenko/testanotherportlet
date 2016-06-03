9fe5c05faa68e9de7c0522d058d0e1728603cc19
YakimKY <tech@inquarta.ru>
2016-06-03 11:33:45 +0300

    first commit

---
+++ src/main/java/com/vottvoydom/portlets/testanotherportlet/TestanotherportletPortletController.java
@@ -0,0 +1,30 @@
+package com.vottvoydom.portlets.testanotherportlet;
+
+import java.net.MalformedURLException;
+import java.util.Arrays;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+
+@Controller("TestanotherportletPortletApp")
+@RequestMapping({"VIEW"})
+public class TestanotherportletPortletController
+{
+	@Autowired
+	private ApplicationContext _applicationContext;
+	
+	
+	@RenderMapping
+	public String handleRenderRequest(RenderRequest request, RenderResponse response, Model model)
+	    throws MalformedURLException {
+		return "TestanotherportletViewPage";
+	}
+}
